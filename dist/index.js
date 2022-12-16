4e743ce6e40227565eb1903f098d088fa2bc4dc7
Animasso Sidibe <asidibe017@gmail.com>
2022-12-16 16:33:57 +0100

    changement example

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Modal", {
-  enumerable: true,
-  get: function get() {
-    return _Modal.Modal;
-  }
-});
+exports.default = void 0;
 var _Modal = require("./Modal");
+var _default = _Modal.Modal;
+exports.default = _default;
