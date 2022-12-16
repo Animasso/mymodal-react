1c9bd6fecf0026b325a31b5ec35407bb1c222f02
Animasso Sidibe <asidibe017@gmail.com>
2022-12-16 18:35:53 +0100

    add

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _Modal = require("./Modal");
+var _default = _Modal.Modal;
+exports.default = _default;
